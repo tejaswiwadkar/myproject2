d3d17ae413bc47793283894731430d69d07f21f5
tejaswiwadkar <143006972+tejaswiwadkar@users.noreply.github.com>
2024-03-06 21:14:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ let nameField=document.querySelector('.namefield');
 let title=document.querySelector('.title');
 let underline=document.querySelector('.underline');
 let forgetPwd = document.querySelector(' form p');
-let signUpForm = document.getElementById('signupform');
-let forgetPasswordForm = document.getElementById('forgetPasswordForm');
-let forgetPasswordLink = document.getElementById('forgetPasswordLink');
-let returnToSignupLink = document.getElementById('returnToSignupLink');
 
 signIn.addEventListener('click',()=>{
     nameField.style.maxHeight='0';
@@ -26,18 +22,3 @@ signUp.addEventListener('click',()=>{
     underline.style.transform='translateX(0)';
     forgetPwd.style.display ='block';
 })
-
-forgetPasswordLink.addEventListener('click', () => {
-    signUpForm.style.display = 'none';
-    forgetPasswordForm.style.display = 'block';
-    title.innerHTML='Forget Password';
-    underline.style.transform='translateX(-50px)';
-});
-
-returnToSignupLink.addEventListener('click', () => {
-    signUpForm.style.display = 'block';
-    forgetPasswordForm.style.display = 'none';
-    underline.style.transform='translateX(0)';
-    title.innerHTML='Sign Up';
-
-});
